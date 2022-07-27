24039c13e40114e015624117debb0ba8f75a7ebf
Hema <hemasahu019@gmail.com>
2022-07-27 12:28:11 +0530

    [Hema Added Welcome to Address Book in main]

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Address_Book
+{
+    public class Program
+    {   
+        //An Entrypoint of console App
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.ReadLine();
+        }
+    }
+}
