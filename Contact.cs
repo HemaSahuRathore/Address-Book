410acf6148547f240148068610b513e1f8386a42
Hema <hemasahu019@gmail.com>
2022-07-27 12:45:36 +0530

    [Hema Added UC1 Code for ability to create contact to AddressBook]

---
+++ Contact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    public class Contact
+    {
+        //Variables
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zipCode;
+        public long phoneNumber;
+        public string email;
+
+        public Contact()
+        {
+            Console.WriteLine("Please enter the contact details:");
+            Console.Write("First Name: ");
+            this.firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            this.lastName = Console.ReadLine();
+            Console.Write("Address: ");
+            this.address = Console.ReadLine();
+            Console.Write("City: ");
+            this.city = Console.ReadLine();
+            Console.Write("State: ");
+            this.state = Console.ReadLine();
+            Console.Write("Zip Code: ");
+            this.zipCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Phone Number: ");
+            this.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email: ");
+            this.email = Console.ReadLine();
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿namespace Address_Book
-{
+{  /// <summary>
+   /// UC1 : Ability to create Contact in Address Book
+   /// </summary>
     public class Program
     {   
         //An Entrypoint of console App
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Welcome to Address Book Program"); //Welcome Message
             Console.ReadLine();
+
+            Contact contact = new Contact(); //creating contact for Address Book
         }
     }
 }
