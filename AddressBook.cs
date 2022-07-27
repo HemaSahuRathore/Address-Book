c73859a7ec9f1044d1146cffcb327c64b0f4a137
Hema <hemasahu019@gmail.com>
2022-07-27 12:57:50 +0530

    [Hema Added UC2 Code to add new contact to Address Book]

---
+++ AddressBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    public class AddressBook
+    {
+        List<Contact> addressBook; //Declaring addressBook
+
+        public AddressBook()
+        {
+            addressBook = new List<Contact>(); //Creating addressBook list to store contact of Contact Class type
+        }
+
+        //Method to add contact
+        public void AddContact()
+        {
+            Contact contact = new Contact(); //Creating object of Contact Class to create a new contact
+            this.addressBook.Add(contact); //Adding object to addressBook List
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -7,10 +7,28 @@
         //An Entrypoint of console App
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program"); //Welcome Message
-            Console.ReadLine();
+            int option;
 
-            Contact contact = new Contact(); //creating contact for Address Book
+            AddressBook addressBookObj = new AddressBook(); //Creating AddressBook Object
+
+            Console.WriteLine("Welcome to Address Book Program"); //Welcome Message
+            
+            //Loop to continue adding contact until user wants to exit
+            do
+            {
+                Console.WriteLine("\nEnter from the below options:");
+                Console.WriteLine("1. Add new contact");
+                Console.WriteLine("2. Exit");
+
+                option = Convert.ToInt32(Console.ReadLine());
+
+                if (option == 1)
+                    addressBookObj.AddContact(); //Calling AddressBook AddContact method using AddressBook object
+                else if(option != 2)
+                    Console.WriteLine("Enter valid option");
+
+
+            } while (option != 2); //continue till user wants to add contact
         }
     }
 }
